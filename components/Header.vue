979c4761f83934a9d327cea2ff3b4b34e4d72451
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-05-02 17:23:57 +0200

    Done

---
+++ components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>
+        <slot>
+            Title from component
+        </slot>    
+    </h1>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+    const newTask = ref('');
+    const tasks = ref([]);
+
+    const addTask = () => {
+        tasks.value.push(newTask.value);
+        newTask.value = '';
+    };
+    
+    const removeTask = (index) => {
+        tasks.value.splice(index, 1);
+    };
+
+
+</script>
+
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <form @submit.prevent="addTask">
+      <input v-model="newTask" type="text" placeholder="Enter a todo" />
+      <button type="submit">Add</button>
+    </form>
+    <ul>
+        <div v-if="tasks.length < 10">
+            <li v-for="(task, index) in tasks" :key="index">
+                {{ task }}
+            <button @click="removeTask(index)">Delete</button>
+            </li>
+        </div>
+        <div v-else>
+          <p>Trop de tâches ! 🧨🧨</p>
+        </div>
+    </ul>
+  </div>
+</template>
